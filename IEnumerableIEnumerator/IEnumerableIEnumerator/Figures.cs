292cda903a60094a988c44c27fd84a7001b1825b
Георгий Бербат <invariantcalibration@gmail.com>
2021-02-24 11:30:31 +0200

    IEnumerable refactor

---
+++ IEnumerableIEnumerator/IEnumerableIEnumerator/Figures.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+
+namespace IEnumerableIEnumerator
+{
+    public class Figures : IEnumerable
+    {
+        private GeometricFigure[] _figures;
+
+        public Figures(GeometricFigure[] items)
+        {
+            _figures = new GeometricFigure[items.Length];
+            for (int i = 0; i < items.Length; i++)
+            {
+                _figures[i] = items[i];
+            }
+        }
+        
+        //IEnumerator IEnumerable.GetEnumerator() => (IEnumerator) GetEnumerator();
+        
+        public IEnumerator GetEnumerator() => new GeometricFigureEnumerator(_figures);
+        
+    }
+}
